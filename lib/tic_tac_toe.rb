5d01b1f13bb6a26eb8dfd549c1420d6685d11d94
JC <bonassin@gmail.com>
2020-12-11 18:11:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,8 +126,8 @@ def play(board)
   end
 
   if won?(board)
-    puts "Congratulations yay! #{winner(board)}!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
-    puts "Cat's Game!"
+    puts "Cats Game!"
   end
 end
