ce736f36195b8537683744fb27d8e0acf1e19e16
JC <bonassin@gmail.com>
2020-12-11 17:30:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,3 +60,15 @@ def current_player(board)
     return "O"
   end
 end
+
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
